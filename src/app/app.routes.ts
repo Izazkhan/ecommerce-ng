96811919006aabc636971d3a6ea9ec298d25efde
IzazKhan <izazkhan134@gmail.com>
2024-06-27 22:22:06 +0500

    Router in progress...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,26 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { AppLayoutComponent } from './layouts/app-layout/app-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: AuthLayoutComponent,
+        children: [
+            {
+                path: 'login', component: LoginComponent
+            }
+        ]
+    },
+    {
+        path: '',
+        component: AppLayoutComponent,
+        children: [
+            {
+                path: '', component: HomeComponent
+            }
+        ]
+    }
 ];
