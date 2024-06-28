f3868c90f1ac53136cfbcc30bda6691c4e89fe0c
IzazKhan <izazkhan134@gmail.com>
2024-06-28 17:50:23 +0500

    Registration functionality in progress...

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  name = new FormControl('');
+  email = new FormControl('');
+  password = new FormControl('');
 
 }
